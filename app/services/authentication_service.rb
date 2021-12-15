7fa76c636615bb111843a78b760204798a7e68cf
Muntaqim Rahman <m222rahman@uwaterloo.ca>
2021-12-15 00:54:06 -0500

    Added getting keys from prod env file

--- app/services/authentication_service.rb
+++ app/services/authentication_service.rb
@@ -1,18 +1,18 @@
 require 'jwt'
 
 class AuthenticationService
-    HMAC_SECRET_KEY = Rails.application.secrets.HMAC_SECRET_KEY
+    KEY_BASE = Rails.application.secrets.secret_key_base
     ALGORITHM_TYPE = 'HS512'
 
     def self.encode(user_id)
         payload = {user_id: user_id}
 
-        JWT.encode payload, HMAC_SECRET_KEY, ALGORITHM_TYPE  
+        JWT.encode payload, KEY_BASE, ALGORITHM_TYPE  
     end
 
     def self.decode(token)
 
-        decoded_token = JWT.decode token,HMAC_SECRET_KEY, true, { algorithm: ALGORITHM_TYPE}
+        decoded_token = JWT.decode token,KEY_BASE, true, { algorithm: ALGORITHM_TYPE}
 
         return decoded_token[0]['user_id']
 
