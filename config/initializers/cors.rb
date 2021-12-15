7057bffba40a3b530832ce32a3e4306d418d8dbb
Muntaqim Rahman <m222rahman@uwaterloo.ca>
2021-12-15 01:26:17 -0500

    Modifed cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://muntaqim-website-frontend.herokuapp.com/'
+    origins 'https://muntaqim-website-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
